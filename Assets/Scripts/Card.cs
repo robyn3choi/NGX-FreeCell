9266debe1431c4d675ffd64fc52a45e9d7d0d228
robyn <robyn3choi@gmail.com>
2019-06-06 16:01:27 -0700

    initial commit. created cards

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    int number;
+    int suit;
+
+    public void Initialize(int number, int suit, Sprite suitSprite)
+    {
+        this.number = number;
+        this.suit = suit;
+        GetComponentInChildren<Text>().text = GetStringFromNumber(number);
+        GetComponentsInChildren<Image>()[1].sprite = suitSprite;
+    }
+
+    private string GetStringFromNumber(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                return "A";
+            case 11:
+                return "J";
+            case 12:
+                return "Q";
+            case 13:
+                return "K";
+            default:
+                return number.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public Transform canvas;
+    public GameObject cardGO;
+    public List<Sprite> suitSprites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Initialize();
+    }
+
+    private void Initialize()
+    {
+        for (int i=1; i<14; i++)
+        {
+            for (int j=0; j<4; j++) // for each suit
+            {
+                Instantiate(cardGO, canvas);
+                Card card = cardGO.GetComponent<Card>();
+                card.Initialize(i, j, suitSprites[j]);
+            }
+        }
+    }
+}
