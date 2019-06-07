150a74d0691b4eb47f09b5e54e3916d0fb6bb126
robyn <robyn3choi@gmail.com>
2019-06-06 17:08:39 -0700

    placed cells in scene, cards get shuffled into tableau

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -5,24 +5,68 @@ using UnityEngine;
 public class CardManager : MonoBehaviour
 {
     public Transform canvas;
-    public GameObject cardGO;
+    public GameObject cardPrefab;
     public List<Sprite> suitSprites;
+    public List<Transform> tableauPiles;
+    
+    private List<Card> cards = new List<Card>();
+    private int currentTableauIndex = 0;
+    private int numCardsInCurrentTableauPile = 0;
+    private const float cardOffset = -26;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         Initialize();
     }
 
     private void Initialize()
     {
+        // create all the cards
         for (int i=1; i<14; i++)
         {
             for (int j=0; j<4; j++) // for each suit
             {
-                Instantiate(cardGO, canvas);
+                GameObject cardGO = Instantiate(cardPrefab);
                 Card card = cardGO.GetComponent<Card>();
                 card.Initialize(i, j, suitSprites[j]);
+                cards.Add(card);
+            }
+        }
+        
+        // make a list of references to each card
+        List<Card> availableCards = new List<Card>();
+        foreach(Card card in cards)
+        {
+            availableCards.Add(card);
+        }
+        // randomly put cards into tableau
+        for (int i=0; i<52; i++)
+        {
+            int randomIndex = Random.Range(0, availableCards.Count);
+            Card randomCard = availableCards[randomIndex];
+            AddCardToTableau(randomCard);
+            availableCards.RemoveAt(randomIndex);
+        }
+    }
+
+    private void AddCardToTableau(Card card)
+    {
+        if (currentTableauIndex == tableauPiles.Count)
+        {
+            print("too many cards, not enough piles!");
+        }
+        else
+        {
+            Transform pile = tableauPiles[currentTableauIndex];
+            card.transform.SetParent(pile);
+            card.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, cardOffset * numCardsInCurrentTableauPile, 0);
+            numCardsInCurrentTableauPile++;
+            if (currentTableauIndex < 4 && numCardsInCurrentTableauPile == 7 ||
+                currentTableauIndex >= 4 && numCardsInCurrentTableauPile == 6)
+            {
+                currentTableauIndex++;
+                numCardsInCurrentTableauPile = 0;
             }
         }
     }
